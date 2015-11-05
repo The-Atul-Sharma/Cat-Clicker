82df74e3978c1eeaaa04f047ab0350aa2e6ca17e
Atul Sharma <theatsharma@gmail.com>
2015-11-05 11:47:30 +0530

    Cat Clicker

---
+++ app.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	var count = 0;
+	var count1 = 0;
+$('#img').click(function() {
+	count++;
+  $('#cat-count').text(count);
+});
+$('#img1').click(function() {
+	count1++;
+  $('#cat-count1').text(count1);
+});
+});
